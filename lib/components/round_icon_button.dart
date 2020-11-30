ba9c29fbe0c1116e9efd4c3bb7c16a36c7e65af1
Juan Navas <navascruzf@gmail.com>
2020-11-30 09:18:04 -0400

    handled long press

--- lib/components/round_icon_button.dart
+++ lib/components/round_icon_button.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
 
-class RoundIconButton extends StatelessWidget {
-
+class RoundIconButton extends StatefulWidget {
   RoundIconButton({this.icon, @required this.onPress});
 
   final IconData icon;
   final Function onPress;
 
   @override
+  _RoundIconButtonState createState() => _RoundIconButtonState();
+}
+
+class _RoundIconButtonState extends State<RoundIconButton> {
+  bool _buttonPressed = false;
+  bool _loopActive = false;
+
+  void _increaseCounterWhilePressed() async {
+    // make sure that only one loop is active
+    if (_loopActive) return;
+
+    _loopActive = true;
+
+    while (_buttonPressed) {
+      // do your thing
+      widget.onPress();
+
+      // wait a bit
+      await Future.delayed(Duration(milliseconds: 100));
+    }
+
+    _loopActive = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPress,
-      // elevation: 6.0,
-      elevation: 2.0,
-      constraints: BoxConstraints.tightFor(
-          width: 56.0,
-          height: 56.0
+    return Listener(
+      child: RawMaterialButton(
+        child: Icon(widget.icon),
+        onPressed: () {},
+        // elevation: 6.0,
+        elevation: 2.0,
+        constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
+        shape: CircleBorder(),
+        // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        fillColor: Color(0xFF4C4F5E),
       ),
-      shape: CircleBorder(),
-      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      fillColor: Color(0xFF4C4F5E),
+      onPointerDown: (details) {
+        _buttonPressed = true;
+        _increaseCounterWhilePressed();
+      },
+      onPointerUp: (details) {
+        _buttonPressed = false;
+        // onPress();
+      },
     );
   }
 }
